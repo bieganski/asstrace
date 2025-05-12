523229b1d5b2529a15e31fb12a49913827fd0f39
Mateusz Bieganski <bieganski.m@wp.pl>
2025-05-12 18:13:41 +0200

    tools/pid_vaddr2line.py: auto-detect ELF, no need to pass from command line.

--- tools/pid_vaddr2line.py
+++ tools/pid_vaddr2line.py
@@ -9,7 +9,6 @@ sys.path.append(str(Path(__file__).parent.parent.resolve()))
 
 from find_pattern_offset import file_pattern_match_find_single_offset
 from objdump_wrapper import system_invoke_objdump_or_use_cached, parse_objdump_output, addr2line
-from asstrace import _read_or_write_process_memory
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,19 +19,57 @@ def run_shell(cmd: str) -> tuple[str, str]:
         raise ValueError(f"Command <{cmd}> exited with {ecode}")
     return stdout, stderr
 
-def pid_read_enough_to_identify(pid: int, vaddr: int) -> bytes:
-    num_bytes_enough = 32
-    return _read_or_write_process_memory(address=vaddr, size=num_bytes_enough, data=bytes(), pid=pid, do_write=False)
+Map_proc_pid_maps = dict[tuple[int, int], tuple[Path, int]]
 
-def main(pid: int, vaddr: int, elf: Path, objdump_executable: Path):
-    if not elf.exists():
-        raise ValueError(f"{elf} does not exist")
+def system_proc_pid_maps_get_regions(pid: int) -> dict[tuple[int, int], tuple[Path, int]]:
+    """
+    NOTE: whenever anything more functionality from /proc/PID/maps will be needed,
+    # please do not reinvent the wheel and use proper parsing, for example https://gist.github.com/fxthomas/3c915909bbf84bc14782cb6adef0f915 instead.
 
-    # signature matching is low effort (and error-prone) solution - proper one should parse /proc/pid/maps.
-    signature = pid_read_enough_to_identify(pid=pid, vaddr=vaddr)
-    file_offset = file_pattern_match_find_single_offset(content=elf.read_bytes(), pattern=signature)
+    returns a mapping from (start, end) to (path, file offset).
+    """
+    res = dict()
+    lines = Path(f"/proc/{pid}/maps").read_text().splitlines()
+    for l in lines:
+        tokens = l.split()
+        if len(tokens) < 6:
+            continue # don't care about anonymous mappings
+        start, end = tokens[0].split("-")
+        start, end = int(start, 16), int(end, 16)
+        offset = int(tokens[2], 16)
+        # XXX: here we assume path not to contain spaces
+        res[(start, end)] = (Path(tokens[-1]), offset)
+    return res
 
-    objdump_raw_output = system_invoke_objdump_or_use_cached(objdump_executable=objdump_executable, elf_file=elf)
+def find_offset_within_elf(map: Map_proc_pid_maps, vaddr: int) -> tuple[Path, int]:
+    """
+    for a given @vaddr and /proc/PID/maps @map representation, returns both a path to ELF whose segment was mmapped and contains @offset
+    and a file offset corresponding to @vaddr.
+    """
+    matches = []
+    for region in map:
+        start, end = region
+        if start <= vaddr < end:
+            matches.append(region)
+
+    if len(matches) != 1:
+        raise RuntimeError(f"Was expecting exactly one match, got {len(matches)} instead")
+
+    region = matches[0]
+    start, end = region
+    elf, file_offset = map[region]
+
+    # @start corresponds to @file_offset, and we are @(vaddr - start) bytes above.
+    seeked_file_offset = file_offset + (vaddr - start)
+    logging.info(f"vaddr={hex(vaddr)} corresponds to file_off={hex(seeked_file_offset)} in {elf}")
+    return elf, seeked_file_offset
+
+
+def main(pid: int, vaddr: int, objdump_executable: Path):
+
+    vaddr_elf, file_offset = find_offset_within_elf(map=system_proc_pid_maps_get_regions(pid=pid), vaddr=vaddr)
+
+    objdump_raw_output = system_invoke_objdump_or_use_cached(objdump_executable=objdump_executable, elf_file=vaddr_elf)
     objdump_parsed_output = parse_objdump_output(objdump_raw_output)
 
     f = addr2line(file_offset=file_offset, objdump_output=objdump_parsed_output)
@@ -43,6 +80,5 @@ if __name__ == "__main__":
     parser = ArgumentParser(usage="XXX")
     parser.add_argument("pid", type=int)
     parser.add_argument("vaddr", type=lambda x: int(x, 16))
-    parser.add_argument("elf", type=Path)
     parser.add_argument("-d", "--objdump-executable", type=Path, help="path to 'objdump' executable, useful if using one from cross-toolchain (e.g. RISC-V)", default="objdump")
     main(**vars(parser.parse_args()))
